b46f6def4f083f8a5b0a7101bea91e65f9a3c98d
santannajp <sanannajp@gmail.com>
2026-05-18 21:14:58 -0300

    construindo docker-compose, setando .env e criando configs com pydantic

---
+++ app/core/config.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+   OPENAI_API_KEY: str
+   DATABASE_URL: str
+   QDRANT_URL: str
+   
+   class Config:
+       env_file = ".env"
+
+
+settings = Settings()
--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+#criando api basica para testar se está funcionando e rodando com uvicorn
 @app.get("/")
 def health():
     return {"status": "ok"}
